8c01ea67fe16c1ac9750270f803ea502f5282100
Apex Yu <apex.yu@emc.com>
2018-02-12 16:58:24 +0800

    resolve Cannot find module '@ngrx/core/compose'

--- src/client/app/modules/ngrx/index.ts
+++ src/client/app/modules/ngrx/index.ts
@@ -12,7 +12,7 @@ import '@ngrx/core/add/operator/select';
  *
  * More: https://drboolean.gitbooks.io/mostly-adequate-guide/content/ch5.html
  */
-import { compose } from '@ngrx/core/compose';
+import { compose } from '@ngrx/store';
 
 /**
  * storeFreeze prevents state from being mutated. When mutation occurs, an
