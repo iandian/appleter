b3995ff236fc18b311a62994d10d36c0b1c2e51a
Apex Yu <apex.yu@emc.com>
2018-04-19 17:32:53 +0800

    first

--- src/client/web.module.ts
+++ src/client/web.module.ts
@@ -1,11 +1,10 @@
-// angular
 import { NgModule } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { Http } from '@angular/http';
 
-// libs
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { DBModule } from '@ngrx/db';
@@ -14,83 +13,44 @@ import {
   RouterStateSerializer,
 } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { TranslateLoader } from '@ngx-translate/core';
 
-// app
-import { APP_COMPONENTS, AppComponent } from './app/components/index';
-import { routes } from './app/components/app.routes';
-import { schema } from './app/modules/db';
+import { CoreModule } from './app/core/core.module';
+import { AuthModule } from './app/auth/auth.module';
 
-// feature modules
-import { WindowService, StorageService, ConsoleService, createConsoleTarget, provideConsoleTarget, LogTarget, LogLevel, ConsoleTarget } from './app/modules/core/services/index';
-import { CoreModule, Config } from './app/modules/core/index';
-import { AnalyticsModule } from './app/modules/analytics/index';
-import { MultilingualModule, Languages, translateLoaderFactory, MultilingualEffects } from './app/modules/i18n/index';
-import { SampleModule, SampleEffects } from './app/modules/sample/index';
-import { reducers } from './app/modules/ngrx/index';
-import { CustomRouterStateSerializer } from './app/modules/shared/utils';
+import { routes } from './app/routes';
+import { reducers, metaReducers } from './app/reducers';
+import { schema } from './app/db';
+import { CustomRouterStateSerializer } from './app/shared/utils';
 
-// config
-Config.PLATFORM_TARGET = Config.PLATFORMS.WEB;
-if (String('<%= BUILD_TYPE %>') === 'dev') {
-  // only output console logging in dev mode
-  Config.DEBUG.LEVEL_4 = true;
-}
+import { AppComponent } from './app/core/containers/app';
 
 let routerModule = RouterModule.forRoot(routes);
 
 if (String('<%= TARGET_DESKTOP %>') === 'true') {
-  Config.PLATFORM_TARGET = Config.PLATFORMS.DESKTOP;
   // desktop (electron) must use hash
   routerModule = RouterModule.forRoot(routes, { useHash: true });
 }
 
-declare var window, console, localStorage;
-
-// For AoT compilation to work:
-export function win() {
-  return window;
-}
-export function storage() {
-  return localStorage;
-}
-export function cons() {
-  return console;
-}
-export function consoleLogTarget(consoleService: ConsoleService) {
-  return new ConsoleTarget(consoleService, { minLogLevel: LogLevel.Debug });
-}
-
-let DEV_IMPORTS: any[] = [];
-
-if (String('<%= BUILD_TYPE %>') === 'dev') {
-  DEV_IMPORTS = [
-    ...DEV_IMPORTS,
-    StoreDevtoolsModule.instrument({
-      name: 'NgRx Book Store DevTools'
-    })
-  ];
-}
-
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
-    CoreModule.forRoot([
-      { provide: WindowService, useFactory: (win) },
-      { provide: StorageService, useFactory: (storage) },
-      { provide: ConsoleService, useFactory: (cons) },
-      { provide: LogTarget, useFactory: (consoleLogTarget), deps: [ConsoleService], multi: true }
-    ]),
+    BrowserAnimationsModule,
+    HttpClientModule,
     routerModule,
-    AnalyticsModule,
-    MultilingualModule.forRoot([{
-      provide: TranslateLoader,
-      deps: [Http],
-      useFactory: (translateLoaderFactory)
-    }]),
-    SampleModule,
-    // configure app state
-    StoreModule.forRoot(reducers),
+
+    /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.forRoot(reducers, { metaReducers }),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store.
+     */
     StoreRouterConnectingModule.forRoot({
       /*
         They stateKey defines the name of the state used by the router-store reducer.
@@ -98,27 +58,48 @@ if (String('<%= BUILD_TYPE %>') === 'dev') {
       */
       stateKey: 'router',
     }),
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Book Store DevTools',
+    }),
+
+    /**
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
+     */
     EffectsModule.forRoot([]),
+
+    /**
+     * `provideDB` sets up @ngrx/db with the provided schema and makes the Database
+     * service available.
+     */
     DBModule.provideDB(schema),
-    // dev environment only imports
-    DEV_IMPORTS,
-  ],
-  declarations: [
-    APP_COMPONENTS
+
+    CoreModule.forRoot(),
+
+    AuthModule.forRoot(),
   ],
   providers: [
+    /**
+     * The `RouterStateSnapshot` provided by the `Router` is a large complex structure.
+     * A custom RouterStateSerializer is used to parse the `RouterStateSnapshot` provided
+     * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
+     */
     { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
-    {
-      provide: APP_BASE_HREF,
-      useValue: '<%= APP_BASE %>'
-    },
-    // override with supported languages
-    {
-      provide: Languages,
-      useValue: Config.GET_SUPPORTED_LANGUAGES()
-    }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class WebModule { }
+export class WebModule {}
